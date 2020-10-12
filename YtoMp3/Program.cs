e01bf4cbe3e5c7d55b9ab5d053caa0454470bc7c
olgrea <olgrea@gmail.com>
2020-10-12 18:06:32 -0400

    convert a video to mp3 using ffmpeg

--- YtoMp3/Program.cs
+++ YtoMp3/Program.cs
@@ -2,9 +2,6 @@
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using YoutubeExplode;
-using YoutubeExplode.Videos;
-using YoutubeExplode.Videos.Streams;
 
 namespace YtoMp3
 {
@@ -19,50 +16,8 @@ namespace YtoMp3
 
             var url = args[0];
 
-            var youtube = new YoutubeClient();
-            var videoId = new VideoId(url);
-
-            var streams = await youtube.Videos.Streams.GetManifestAsync(videoId);
-            var streamInfo = streams.GetAudioOnly().WithHighestBitrate();
-            if (streamInfo == null)
-            {
-                Console.Error.WriteLine("This videos has no streams");
-            }
-            
-            var fileName = $"{videoId}.{streamInfo.Container.Name}";
-
-            // Download video
-            Console.Write($"Downloading stream: {streamInfo.Url} / {streamInfo.Container.Name}... ");
-            using (var progress = new InlineProgress())
-            {
-                await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName, progress);
-            }
-
-            Console.WriteLine($"Video saved to '{fileName}'");
-        }
-    }
-
-    internal class InlineProgress : IProgress<double>, IDisposable
-    {
-        private readonly int _posX;
-        private readonly int _posY;
-
-        public InlineProgress()
-        {
-            _posX = Console.CursorLeft;
-            _posY = Console.CursorTop;
-        }
-
-        public void Report(double progress)
-        {
-            Console.SetCursorPosition(_posX, _posY);
-            Console.WriteLine($"{progress:P1}");
-        }
-
-        public void Dispose()
-        {
-            Console.SetCursorPosition(_posX, _posY);
-            Console.WriteLine("Completed ✓");
+            var client = new YtoMp3();
+            await client.Download(url);
         }
     }
 }
---
+++ YtoMp3/YtoMp3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
+
+namespace YtoMp3
+{
+    public class YtoMp3
+    {
+        private YoutubeClient _youtube;
+
+        public YtoMp3()
+        {
+            _youtube = new YoutubeClient();
+        }
+
+        public string ReplaceInvalidChars(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+
+        public async Task Download(string idOrUrl)
+        {
+            VideoId id = new VideoId(idOrUrl);
+
+            var videoInfo = await _youtube.Videos.GetAsync(id);
+            var manifest = await _youtube.Videos.Streams.GetManifestAsync(id);
+            if (manifest == null)
+            {
+                throw new ArgumentException("no manifest found");
+            }
+
+            var stream = manifest.GetAudioOnly().WithHighestBitrate();
+            if (stream == null)
+            {
+                throw new ArgumentException("no audio stream found");
+            }
+
+            var tmpFile = $"{id.Value}.{stream.Container.Name}";
+            if (File.Exists(tmpFile))
+                File.Delete(tmpFile);
+
+            Console.WriteLine($"Downloading video {id}...");
+            using (var progress = new  InlineProgress())
+            {
+                await _youtube.Videos.Streams.DownloadAsync(stream, tmpFile, progress);
+            }
+
+            var tmpFileInfo = new FileInfo(tmpFile);
+            if (!tmpFileInfo.Exists || tmpFileInfo.Length == 0)
+            {
+                throw new ArgumentException($"problem during download of video {id}.");
+            }
+
+            // TODO : set as embedded
+            FFmpeg.SetExecutablesPath(Directory.GetCurrentDirectory());
+
+            var mediaInfo = await FFmpeg.GetMediaInfo(tmpFile);
+            var outputFilename = ReplaceInvalidChars($"{videoInfo.Title}.mp3");
+            if (File.Exists(outputFilename))
+                File.Delete(outputFilename);
+
+            var audioStream = mediaInfo.AudioStreams.FirstOrDefault();
+
+            var conversion = FFmpeg.Conversions.New();
+            conversion.AddStream(audioStream);
+            conversion.SetAudioBitrate(audioStream.Bitrate);
+            conversion.SetOutput(outputFilename);
+
+            Console.WriteLine($"Converting {id} to mp3...");
+            var convProgress = new InlineProgress();
+            conversion.OnProgress += (sender, args) => { convProgress.Report(args.Percent); };
+            await conversion.Start();
+            convProgress.Dispose();
+
+            var outputFileInfo = new FileInfo(outputFilename);
+            if (!outputFileInfo.Exists || outputFileInfo.Length == 0)
+            {
+                throw new ArgumentException($"problem during conversion of video {id}.");
+            }
+
+            if (File.Exists(tmpFile))
+                File.Delete(tmpFile);
+        }
+    }
+}
