f9444e8ef91e4afb4c5d96e54b247e81d965ec70
olgrea <olgrea@gmail.com>
2020-10-12 15:31:05 -0400

    including external library YoutubeExplode

--- YtoMp3/Program.cs
+++ YtoMp3/Program.cs
@@ -1,31 +1,68 @@
 ﻿using System;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 namespace YtoMp3
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var client = new YoutubeClient();
-            var info = client.GetVideoInfo("https://www.youtube.com/watch?v=fKvk3tpHrdA&list=PL1qgThHfu0PbSTChu1bFaLV2-ijaAZCDY&index=44").Result;
-
-            PrintInfo(info);
-        }
-
-        private static void PrintInfo(VideoInfo info)
-        {
-            if(info == null) return;
-
-            var props = info.GetType().GetProperties();
-            StringBuilder sb = new StringBuilder();
-            foreach (PropertyInfo propertyInfo in props)
+            if (args.Length == 0)
             {
-                sb.AppendLine($"{propertyInfo.Name} : {propertyInfo.GetValue(info)}");
+                Console.WriteLine("Enter video or playlist url");
             }
 
-            Console.WriteLine(sb.ToString());
+            var url = args[0];
+
+            var youtube = new YoutubeClient();
+            var videoId = new VideoId(url);
+
+            var streams = await youtube.Videos.Streams.GetManifestAsync(videoId);
+            var streamInfo = streams.GetAudioOnly().WithHighestBitrate();
+            if (streamInfo == null)
+            {
+                Console.Error.WriteLine("This videos has no streams");
+            }
+            
+            var fileName = $"{videoId}.{streamInfo.Container.Name}";
+
+            // Download video
+            Console.Write($"Downloading stream: {streamInfo.Url} / {streamInfo.Container.Name}... ");
+            using (var progress = new InlineProgress())
+            {
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName, progress);
+            }
+
+            Console.WriteLine($"Video saved to '{fileName}'");
+        }
+    }
+
+    internal class InlineProgress : IProgress<double>, IDisposable
+    {
+        private readonly int _posX;
+        private readonly int _posY;
+
+        public InlineProgress()
+        {
+            _posX = Console.CursorLeft;
+            _posY = Console.CursorTop;
+        }
+
+        public void Report(double progress)
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine($"{progress:P1}");
+        }
+
+        public void Dispose()
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine("Completed ✓");
         }
     }
 }
